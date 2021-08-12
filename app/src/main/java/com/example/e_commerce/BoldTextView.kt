ce9c331e3db6c63b49e9009411f1635c64d8fac0
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-08-11 23:05:46 -0400

    Commit #6 - Added BoldTextView class for custom text widget.

---
+++ app/src/main/java/com/example/e_commerce/BoldTextView.kt
@@ -0,0 +1,17 @@
+package com.example.e_commerce
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class BoldTextView(context: Context, attributeSet: AttributeSet) : AppCompatEditText(context, attributeSet) {
+    init{
+        applyFont()
+    }
+
+    private fun applyFont(){
+        val boldTypeface: Typeface = Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+        typeface = boldTypeface
+    }
+}
