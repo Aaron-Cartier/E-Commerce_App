1e3b01f3e3f4e1de731b62aab6ecef853e398004
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-11-06 23:42:07 -0400

    commit #24 - Finished forgot email activity, now upon entering a valid registered email on the database, user will receive an email to reset their account pass.

--- app/src/main/java/com/example/e_commerce/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/e_commerce/activities/ForgotPasswordActivity.kt
@@ -2,11 +2,15 @@ package com.example.e_commerce.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.e_commerce.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_forgot_password.*
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class ForgotPasswordActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
@@ -24,5 +28,31 @@ class ForgotPasswordActivity : AppCompatActivity() {
         }
 
         toolbar_forgot_password_activity.setNavigationOnClickListener {onBackPressed()}
+
+        btn_submit.setOnClickListener{
+            val email: String = et_email_fp.text.toString().trim {it <= ' '}
+            if(email.isEmpty()) {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+            }else{
+                showProgressDialog(resources.getString(R.string.please_wait))
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener{task ->
+                        hideProgressDialog()
+                        if(task.isSuccessful) {
+                            //show message and finish the forgot password activity to go back to the login screen activity
+                              //showErrorSnackBar(resources.getString(R.string.email_sent_success), false)
+                            Toast.makeText(
+                                this@ForgotPasswordActivity,
+                                resources.getString(R.string.email_sent_success),
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            finish()
+                        }else{
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+            }
+        }
     }
 }
