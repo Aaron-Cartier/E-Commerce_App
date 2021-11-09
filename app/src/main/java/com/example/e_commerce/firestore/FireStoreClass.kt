31a7f15b25150e6fd302cac7026ebf7e99af47d9
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-11-09 16:30:39 -0500

    commit #27 - Created an object file Constants.kt under utils. Made function named getCurrentUserId under FireStoreClass to retrieve user ID from fire base.

--- app/src/main/java/com/example/e_commerce/firestore/FireStoreClass.kt
+++ app/src/main/java/com/example/e_commerce/firestore/FireStoreClass.kt
@@ -3,6 +3,8 @@ package com.example.e_commerce.firestore
 import android.util.Log
 import com.example.e_commerce.activities.RegisterActivity
 import com.example.e_commerce.models.User
+import com.example.e_commerce.utils.Constants
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 
@@ -11,7 +13,7 @@ class FireStoreClass {
 
     fun registerUser(activity: RegisterActivity, userInfo: User) {
         //The "users" is collection name. If the collection is already created then it will not create the same one again.
-        mFireStore.collection("users")
+        mFireStore.collection(Constants.USERS)
             //Document ID for users fields. Here the document it is the user ID.
             .document(userInfo.id)
             // Here the userInfo are Field and the SetOption is set to merge. It is for if we wants to merge later on instead of replacing the fields.
@@ -28,5 +30,17 @@ class FireStoreClass {
                     e
                 )
             }
+    }
+
+    fun getCurrentUserId(): String {
+        //An instance of currentUser using FirebaseAuth
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        //A variable to assign the currentUserId id it is not null or else it will be blank
+        var currentUserId = ""
+        if(currentUser != null) {
+            currentUserId = currentUser.uid
+        }
+        return currentUserId
     }
 }
---
+++ app/src/main/java/com/example/e_commerce/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.e_commerce.utils
+
+object Constants {
+    const val USERS: String = "users"
+
+}
