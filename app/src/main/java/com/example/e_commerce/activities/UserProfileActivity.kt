3142efb1c6e4926e5d40cfb94b374924c16adc9f
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-11-16 19:27:00 -0500

    commit #35 - Added permission to access external storage for user profile pic, when user clicks on the profile pic in profile settings, user will get a prompt to allow or deny permission

--- app/src/main/java/com/example/e_commerce/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/e_commerce/activities/UserProfileActivity.kt
@@ -1,13 +1,19 @@
 package com.example.e_commerce.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.e_commerce.R
 import com.example.e_commerce.models.User
 import com.example.e_commerce.utils.Constants
 import kotlinx.android.synthetic.main.activity_user_profile.*
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
@@ -26,5 +32,52 @@ class UserProfileActivity : AppCompatActivity() {
 
         et_email.isEnabled = false
         et_email.setText(userDetails.email)
+
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null) {
+            when(v.id) {
+                R.id.iv_user_photo -> {
+                    //Here we will check if the permission is already allowed or we need to request for it
+                    //first of all we will check the READ_EXTERNAL_STORAGE permission and if it is allowed we will request for the same.
+                    if(ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        ) == PackageManager.PERMISSION_GRANTED
+                    )
+                    {showErrorSnackBar("You already have the storage permission.", false)
+                    }else{
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //if permission is granted
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showErrorSnackBar("The storage permission is grant", false)
+                }else{
+                    //displaying another toast if persmission is not granted
+                Toast.makeText(this, resources.getString(R.string.read_storage_permission_denied), Toast.LENGTH_LONG).show()
+                }
+        }
     }
 }
