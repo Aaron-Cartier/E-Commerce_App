432bfc8b16848f10e01df823525b028b6e614ed0
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-11-24 14:10:49 -0500

    commit #47 - added SettingsActivity, settings icon, new dashboard menu up top for settings icon

--- app/src/main/java/com/example/e_commerce/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/fragments/DashboardFragment.kt
@@ -1,12 +1,13 @@
 package com.example.e_commerce.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.e_commerce.R
 import com.example.e_commerce.databinding.FragmentDashboardBinding
+import com.example.e_commerce.ui.activities.SettingsActivity
 
 class DashboardFragment : Fragment() {
 
@@ -17,6 +18,11 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //if we want to use the option menu in fragment we need to add it
+        setHasOptionsMenu(true)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,5 +43,22 @@ class DashboardFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when(id) {
+            R.id.action_settings -> {
+                startActivity(Intent(activity, SettingsActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
