96f7255300e190cb1c98464daabc756c0909fe0f
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-11-28 20:13:12 -0500

    commit #56 - user can now upload a product pic from phone, edit button also available if needed change the loaded pic.

--- app/src/main/java/com/example/e_commerce/ui/activities/AddProductActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/activities/AddProductActivity.kt
@@ -1,16 +1,29 @@
 package com.example.e_commerce.ui.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.e_commerce.R
+import com.example.e_commerce.utils.Constants
+import com.example.e_commerce.utils.GlideLoader
 import kotlinx.android.synthetic.main.activity_add_product.*
+import java.io.IOException
 
-class AddProductActivity : AppCompatActivity() {
+class AddProductActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
 
         setupActionBar()
+
+        iv_add_update_product.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
@@ -23,5 +36,80 @@ class AddProductActivity : AppCompatActivity() {
         }
 
         toolbar_add_product_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null) {
+            when(v.id) {
+                // The permission code is similar to the user profile image selection.
+                R.id.iv_add_update_product -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Constants.showImageChooser(this@AddProductActivity)
+                    } else {
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Constants.showImageChooser(this@AddProductActivity)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+
+            // Replace the add icon with edit icon once the image is selected.
+            iv_add_update_product.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this@AddProductActivity,
+                    R.drawable.ic_vector_edit
+                )
+            )
+
+            // The uri of selection image from phone storage.
+            val selectedImageFileURI = data.data!!
+
+            try {
+                // Load the product image in the ImageView.
+                GlideLoader(this@AddProductActivity).loadUserPicture(selectedImageFileURI, iv_product_image)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
