f05c4c03ca4d2b79a297615172ae8560bd1c94f0
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-12-07 18:09:04 -0500

    commit #65 - Products added to the products collection on the fire base by different users are now shown on the products fragment page.

--- app/src/main/java/com/example/e_commerce/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/fragments/DashboardFragment.kt
@@ -6,11 +6,14 @@ import android.util.Log
 import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.e_commerce.R
 import com.example.e_commerce.databinding.FragmentDashboardBinding
 import com.example.e_commerce.firestore.FireStoreClass
 import com.example.e_commerce.models.Product
 import com.example.e_commerce.ui.activities.SettingsActivity
+import com.example.e_commerce.ui.adapters.DashboardItemsListAdapter
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : BaseFragment() {
 
@@ -68,8 +71,19 @@ class DashboardFragment : BaseFragment() {
 
     fun successDashboardItemsList(dashboardItemsList: ArrayList<Product>) {
         hideProgressDialog()
-        for(i in dashboardItemsList) {
-            Log.i("Item title", i.title)
+
+        if(dashboardItemsList.size > 0) {
+            rv_dashboard_items.visibility = View.VISIBLE
+            tv_no_dashboard_items_found.visibility = View.GONE
+
+            rv_dashboard_items.layoutManager = GridLayoutManager(activity,2)
+            rv_dashboard_items.setHasFixedSize(true)
+
+            val  adapter = DashboardItemsListAdapter(requireActivity(), dashboardItemsList)
+            rv_dashboard_items.adapter = adapter
+        }else{
+            rv_dashboard_items.visibility = View.GONE
+            tv_no_dashboard_items_found.visibility = View.VISIBLE
         }
     }
 
