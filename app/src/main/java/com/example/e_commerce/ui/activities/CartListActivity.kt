713717ee65b850fd5597c1e6979a5627a8a5b326
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-12-15 22:13:20 -0500

    commit #79 - Added cart adapter to, items added to cart now show in the carts page, correct total amount is also shown.

--- app/src/main/java/com/example/e_commerce/ui/activities/CartListActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/activities/CartListActivity.kt
@@ -2,9 +2,13 @@ package com.example.e_commerce.ui.activities
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_commerce.R
 import com.example.e_commerce.firestore.FireStoreClass
 import com.example.e_commerce.models.CartItem
+import com.example.e_commerce.ui.adapters.CartItemsListAdapter
+import com.google.api.Distribution
 import kotlinx.android.synthetic.main.activity_cart_list.*
 
 class CartListActivity : BaseActivity() {
@@ -16,7 +20,6 @@ class CartListActivity : BaseActivity() {
     }
 
     private fun setupActionBar() {
-
         setSupportActionBar(toolbar_cart_list_activity)
 
         val actionBar = supportActionBar
@@ -24,13 +27,11 @@ class CartListActivity : BaseActivity() {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
         }
-
         toolbar_cart_list_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
     override fun onResume() {
         super.onResume()
-
         getCartItemsList()
     }
 
@@ -48,47 +49,40 @@ class CartListActivity : BaseActivity() {
         // Hide progress dialog.
         hideProgressDialog()
 
-        for(i in cartList) {
-            Log.i("Cart Item Title", i.title)
-        }
+        if(cartList.size > 0) {
+            rv_cart_items_list.visibility = View.VISIBLE
+            ll_checkout.visibility = View.VISIBLE
+            tv_no_cart_item_found.visibility = View.GONE
 
-//        if (cartList.size > 0) {
-//            rv_cart_items_list.visibility = View.VISIBLE
-//            ll_checkout.visibility = View.VISIBLE
-//            tv_no_cart_item_found.visibility = View.GONE
-//
-//            rv_cart_items_list.layoutManager = LinearLayoutManager(this@CartListActivity)
-//            rv_cart_items_list.setHasFixedSize(true)
-//
-//            val cartListAdapter = CartItemsListAdapter(this@CartListActivity, cartList)
-//            rv_cart_items_list.adapter = cartListAdapter
-//
-//            var subTotal: Double = 0.0
-//
-//            for (item in cartList) {
-//                val price = item.price.toDouble()
-//                val quantity = item.cart_quantity.toInt()
-//                subTotal += (price * quantity)
-//            }
-//
-//            tv_sub_total.setText("$$subTotal")
-//            // Here we have kept Shipping Charge is fixed as $10 but in your case it may cary.
-//            // Also, it depends on the location and total amount.
-//            tv_shipping_charge.setText("$10.0")
-//
-//            if (subTotal > 0) {
-//                ll_checkout.visibility = View.VISIBLE
-//
-//                val total = subTotal + 10
-//                tv_total_amount.setText("$$total")
-//            } else {
-//                ll_checkout.visibility = View.GONE
-//            }
-//
-//        } else {
-//            rv_cart_items_list.visibility = View.GONE
-//            ll_checkout.visibility = View.GONE
-//            tv_no_cart_item_found.visibility = View.VISIBLE
-//        }
+            rv_cart_items_list.layoutManager = LinearLayoutManager(this@CartListActivity)
+            rv_cart_items_list.setHasFixedSize(true)
+            val cartListAdapter = CartItemsListAdapter(this@CartListActivity, cartList)
+            rv_cart_items_list.adapter = cartListAdapter
+
+            var subTotal: Double = 0.0
+
+            for(item in cartList) {
+                val price = item.price.toDouble()
+                val quantity = item.cart_quantity.toInt()
+                subTotal += (price * quantity)
+            }
+
+            tv_sub_total.setText("$$subTotal")
+            tv_shipping_charge.setText("$10.0")
+
+            if(subTotal > 0) {
+                ll_checkout.visibility = View.VISIBLE
+
+                val total = subTotal + 10
+                tv_total_amount.setText("$$total")
+            }else{
+                ll_checkout.visibility = View.GONE
+            }
+        }
+        else{
+            rv_cart_items_list.visibility = View.GONE
+            ll_checkout.visibility = View.GONE
+            tv_no_cart_item_found.visibility = View.VISIBLE
+        }
     }
 }
