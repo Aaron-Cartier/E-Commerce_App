4b201aa853551dc50322a935781bb2b4e0ab0103
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-12-17 16:04:36 -0500

    commit #81 - adding on to the previous cart amount with stock quantity work

--- app/src/main/java/com/example/e_commerce/ui/activities/CartListActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/activities/CartListActivity.kt
@@ -15,6 +15,7 @@ import kotlinx.android.synthetic.main.activity_cart_list.*
 class CartListActivity : BaseActivity() {
 
     private lateinit var mProductsList: ArrayList<Product>
+    private lateinit var mCartListItems: ArrayList<CartItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +42,7 @@ class CartListActivity : BaseActivity() {
     }
 
     private fun getCartItemsList() {
-        showProgressDialog(resources.getString(R.string.please_wait))
+        //showProgressDialog(resources.getString(R.string.please_wait))
         FireStoreClass().getCartList(this@CartListActivity)
     }
 
@@ -59,6 +60,19 @@ class CartListActivity : BaseActivity() {
         // Hide progress dialog.
         hideProgressDialog()
 
+        for(product in mProductsList) {
+            for(cartItem in cartList) {
+                if(product.product_id == cartItem.product_id) {
+                    cartItem.stock_quantity = product.stock_quantity
+
+                    if(product.stock_quantity.toInt() == 0) {
+                        cartItem.cart_quantity = product.stock_quantity
+                    }
+                }
+            }
+        }
+        mCartListItems = cartList
+
         if(cartList.size > 0) {
             rv_cart_items_list.visibility = View.VISIBLE
             ll_checkout.visibility = View.VISIBLE
@@ -71,10 +85,15 @@ class CartListActivity : BaseActivity() {
 
             var subTotal: Double = 0.0
 
-            for(item in cartList) {
-                val price = item.price.toDouble()
-                val quantity = item.cart_quantity.toInt()
-                subTotal += (price * quantity)
+            for(item in mCartListItems) {
+                val availableQuantity = item.stock_quantity.toInt()
+
+                if(availableQuantity > 0) {
+                    val price = item.price.toDouble()
+                    val quantity = item.cart_quantity.toInt()
+                    subTotal += (price * quantity)
+                }
+
             }
 
             tv_sub_total.setText("$$subTotal")
@@ -97,8 +116,8 @@ class CartListActivity : BaseActivity() {
     }
 
     fun successProductsListFromFireStore(productsList: ArrayList<Product>) {
+        hideProgressDialog()
         mProductsList = productsList
-
         getCartItemsList()
     }
 }
