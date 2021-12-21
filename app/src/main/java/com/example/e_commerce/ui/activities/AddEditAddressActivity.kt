07f18a3655a7cd9c8fffae78fccc67a508e188df
Aaron-Cartier <aaron.cartier1@gmail.com>
2021-12-21 18:16:21 -0500

    commit #87 - Added another data model named "Address", made function to check text fields for the address page

--- app/src/main/java/com/example/e_commerce/ui/activities/AddEditAddressActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/activities/AddEditAddressActivity.kt
@@ -2,11 +2,15 @@ package com.example.e_commerce.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import com.example.e_commerce.R
+import com.example.e_commerce.firestore.FireStoreClass
+import com.example.e_commerce.models.Address
+import com.example.e_commerce.utils.Constants
 import kotlinx.android.synthetic.main.activity_add_edit_address.*
 import kotlinx.android.synthetic.main.activity_address_list.*
 
-class AddEditAddressActivity : AppCompatActivity() {
+class AddEditAddressActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit_address)
@@ -25,5 +29,87 @@ class AddEditAddressActivity : AppCompatActivity() {
         }
 
         toolbar_add_edit_address_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun validateData(): Boolean {
+        return when {
+            TextUtils.isEmpty(et_full_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_full_name),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(et_phone_number.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_phone_number),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(et_address.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_address), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_zip_code.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_zip_code), true)
+                false
+            }
+
+            rb_other.isChecked && TextUtils.isEmpty(
+                et_zip_code.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_zip_code), true)
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    /**
+     * A function to save the address to the cloud fire store.
+     */
+    private fun saveAddressToFirestore() {
+
+        // Here we get the text from editText and trim the space
+        val fullName: String = et_full_name.text.toString().trim { it <= ' ' }
+        val phoneNumber: String = et_phone_number.text.toString().trim { it <= ' ' }
+        val address: String = et_address.text.toString().trim { it <= ' ' }
+        val zipCode: String = et_zip_code.text.toString().trim { it <= ' ' }
+        val additionalNote: String = et_additional_note.text.toString().trim { it <= ' ' }
+        val otherDetails: String = et_other_details.text.toString().trim { it <= ' ' }
+
+        if (validateData()) {
+
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val addressType: String = when {
+                rb_home.isChecked -> {
+                    Constants.HOME
+                }
+                rb_office.isChecked -> {
+                    Constants.OFFICE
+                }
+                else -> {
+                    Constants.OTHER
+                }
+            }
+
+            val addressModel = Address(
+                FireStoreClass().getCurrentUserId(),
+                fullName,
+                phoneNumber,
+                address,
+                zipCode,
+                additionalNote,
+                addressType,
+                otherDetails
+            )
+        }
     }
 }
--- app/src/main/java/com/example/e_commerce/utils/Constants.kt
+++ app/src/main/java/com/example/e_commerce/utils/Constants.kt
@@ -41,8 +41,12 @@ object Constants {
     const val CART_ITEMS: String = "cart_items"
 
     const val PRODUCT_ID: String = "product_id"
-
     const val CART_QUANTITY: String = "cart_quantity"
+
+    const val HOME: String = "Home"
+    const val OFFICE: String = "Office"
+    const val OTHER: String = "Other"
+
 
     fun showImageChooser(activity: Activity) {
         //an intent for launching the image selection of phone storage
