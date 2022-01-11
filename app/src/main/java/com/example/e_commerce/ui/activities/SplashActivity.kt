28c2673b3fda70ac82a708fac1102692345ecd0f
Aaron-Cartier <aaron.cartier1@gmail.com>
2022-01-11 16:12:15 -0500

    commit #108 - Sold product details page complete. App is fully functional now and complete.

--- app/src/main/java/com/example/e_commerce/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/activities/SplashActivity.kt
@@ -8,6 +8,7 @@ import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.e_commerce.R
+import com.example.e_commerce.firestore.FireStoreClass
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -27,9 +28,21 @@ class SplashActivity : AppCompatActivity() {
         @Suppress("DEPRECATION")
         Handler().postDelayed(
             {
-                //launches the main activity
-                startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
-                finish() //call this when your activity is done and should be closed.
+                // If the user is logged in once and did not logged out manually from the app.
+                // So, next time when the user is coming into the app user will be redirected to MainScreen.
+                // If user is not logged in or logout manually then user will  be redirected to the Login screen as usual.
+
+                // Get the current logged in user id
+                val currentUserID = FireStoreClass().getCurrentUserId()
+
+                if (currentUserID.isNotEmpty()) {
+                    // Launch dashboard screen.
+                    startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
+                } else {
+                    // Launch the Login Activity
+                    startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                }
+                finish() // Call this when your activity is done and should be closed.
             },
             2500
         )
